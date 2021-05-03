2023ae0fe75976334b28a20f7e87c3e17068fd2a
Oscale <>
2021-05-03 18:14:57 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+document.getElementById('bcolor').addEventListener('click', BgColor)
+
+document.getElementById('cimage').addEventListener('click', ChImage)
+
+document.getElementById('ctext').addEventListener('click', ChText)
+
+document.getElementById('hidetext').addEventListener('click', HText)
+
+document.getElementById('showtext').addEventListener('click', SText)
+
+function BgColor () {
+  document.body.style.backgroundColor = "yellow"
+}
+
+function ChImage () {
+  document.getElementById('changing-image').src = "Hand green thumb.png"
+}
+
+function ChText () {
+  document.getElementById('ptext').innerHTML = "This is now different Text"
+}
+
+function HText () {
+  document.getElementById('ptext').style.display = "none"
+}
+
+function SText () {
+  document.getElementById('ptext').style.display = "block"
+}
