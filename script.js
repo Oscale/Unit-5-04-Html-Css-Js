5c244c6e3c556aeeb7847e94396a690b3d2edf81
Oscar A.G <>
2021-05-03 18:22:13 +0000

    Fixed Jss and Html

--- script.js
+++ script.js
@@ -9,21 +9,21 @@ document.getElementById('hidetext').addEventListener('click', HText)
 document.getElementById('showtext').addEventListener('click', SText)
 
 function BgColor () {
-  document.body.style.backgroundColor = "yellow"
+  document.body.style.backgroundColor = 'yellow'
 }
 
 function ChImage () {
-  document.getElementById('changing-image').src = "Hand green thumb.png"
+  document.getElementById('changing-image').src = 'Hand green thumb.png'
 }
 
 function ChText () {
-  document.getElementById('ptext').innerHTML = "This is now different Text"
+  document.getElementById('ptext').innerHTML = 'This is now different Text'
 }
 
 function HText () {
-  document.getElementById('ptext').style.display = "none"
+  document.getElementById('ptext').style.display = 'none'
 }
 
 function SText () {
-  document.getElementById('ptext').style.display = "block"
+  document.getElementById('ptext').style.display = 'block'
 }
